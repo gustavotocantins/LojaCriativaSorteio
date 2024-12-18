c8a51656b06d851c6aff59bbce1f30ba05349a27
Gustavo Tocantins <64499085+gustavotocantins@users.noreply.github.com>
2024-12-18 19:44:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // Serve arquivos estáticos na pasta public
 app.use(express.static(path.join(__dirname, 'public')));
