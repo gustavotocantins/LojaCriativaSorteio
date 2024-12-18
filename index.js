ff7eb27b3e2c0ffb4df2e31cd039c09ddb9dc68b
Gustavo Tocantins <64499085+gustavotocantins@users.noreply.github.com>
2024-12-18 19:46:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const path = require('path');
 
@@ -7,6 +6,11 @@ const port = process.env.PORT || 80;
 
 // Serve arquivos estáticos na pasta public
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve o arquivo index.html na rota raiz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
